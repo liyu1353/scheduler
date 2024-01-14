9f703a5e088c913b809f29b452dd0784c5d45adb
daniel <xwingpilot15@gmail.com>
2024-01-14 16:54:54 -0600

    begin web side i guess

---
+++ index.js
@@ -0,0 +1,14 @@
+function Top() {
+    return (
+        <div>
+            <h1>
+                Test...
+            </h1>
+        </div>
+    )
+}
+
+function Calendar(){
+    //calendar code i guess
+    
+}
