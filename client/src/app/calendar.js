807f1bd773ff558fe1f9fe4a3a0fe1c686aa5432
daniel <xwingpilot15@gmail.com>
2024-01-21 16:52:36 -0500

    starting sheet reader

--- client/src/app/calendar.js
+++ client/src/app/calendar.js
@@ -1,9 +1,29 @@
 'use client'
 
+import GSheetReader from 'g-sheets-api';
 import React, { Component } from 'react';
 import CalendarDays from "./calendar-days";
 import './calendar.css'
 
+const options = {
+    apiKey: 'AIzaSyD4LnmU8PudTtBQ7JOr60zsltPktF9FtrM',
+    sheetId: '1MdxRxvXXiE8_k_2_wr4_U70fBVt5j4WY137z9Pnj8qQ',
+    sheetNumber: 1,
+    sheetName: 'Scheduler Database', // if sheetName is supplied, this will take precedence over sheetNumber
+    returnAllResults: false
+}
+
+function Retriever() {
+    GSheetReader(
+        options,
+        results => {
+
+            // do something with the results here
+        },
+        error => {
+            // OPTIONAL: handle errors here
+        });
+}
 
 export default class Calendar extends Component {
     constructor() {
@@ -17,6 +37,7 @@ export default class Calendar extends Component {
             currentDay: new Date()
         }
     }
+
     render() {
         return (
             <div className="calendar">
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,8 @@
 import Image from 'next/image'
 
-import GSheetReader from 'g-sheets-api';
 import Calendar from './calendar.js'
 
-const options = {
-  apiKey: 'AIzaSyD4LnmU8PudTtBQ7JOr60zsltPktF9FtrM',
-  sheetId: '1MdxRxvXXiE8_k_2_wr4_U70fBVt5j4WY137z9Pnj8qQ',
-  sheetNumber: 1,
-  sheetName: 'Scheduler Database', // if sheetName is supplied, this will take precedence over sheetNumber
-  returnAllResults: false
-}
+
 function Top() {
   return (
       <button>Test...</button>
